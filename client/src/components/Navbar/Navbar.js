72aee86c5b86420bf43b5b16b091967b4b7f98e0
Marco Amador <marcoamador214@gmail.com>
2023-01-17 22:49:34 -0800

    developed navbar, added calbearsfball logo

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,11 +1,26 @@
 import React from 'react';
-
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import images from '../../constants/images';
 import './Navbar.css';
 
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-);
+function Navbar() {
+  return (
+    <nav className='app_navbar'>
+      <div className='navbar_logo'>
+        <img className='navbar_image' src={images.CalbearsFball} alt='logo'/>
+      </div>
+      <ul className='navbar_links'>
+        <li className='p__opensans'><a href='#home'></a>Home</li>
+        <li className='p__opensans'><a href='#about'></a>About</li>
+        <li className='p__opensans'><a href='#menu'></a>Roster</li>
+        <li className='p__opensans'><a href='#awards'></a>Schedule</li>
+        <li className='p__opensans'><a href='#contact'></a>Contact</li>
+      </ul>
+      <div className='navbar_login'>
+        <a href='#login' className='p__opensans'>Log In/Register</a>
+      </div>
+    </nav>
+  )
+}
 
 export default Navbar;
